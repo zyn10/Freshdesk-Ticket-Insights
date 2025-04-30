d05ccc186319fc8743205183305ae120be18f3d8
Muhammad Zain <zainandcs@gmail.com>
2025-04-30 11:08:52 +0500

    Code Cleaning

--- js/main.js
+++ js/main.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     initializeFileHandlers();
 
     if (isViewerPage()) {
+      initCursor();
       renderTableFromStorage();
     }
   } catch (error) {
